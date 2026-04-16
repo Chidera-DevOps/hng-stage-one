b59a86123be50809ea565da0437846ef4e8974ca
Chidera-DevOps <deradev24@gmail.com>
2026-04-16 19:48:52 +0000

    Initial commit: Stage 1 API

---
+++ main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "API is running"}
+
+@app.get("/health")
+async def health():
+    return {"message": "healthy"}
+
+@app.get("/me")
+async def me():
+    return {
+        "name": "0xdera_24", 
+        "email": "yourname@example.com",
+        "github": "https://github.com/Chidera-DevOps"
+    }
